a130736559210f86beec3ba77f707133c576c146
Nurul Izzah <mephistomum@gmail.com>
2025-10-03 15:46:24 +0800

    update album.js

--- get_album.php
+++ get_album.php
@@ -1,21 +1,21 @@
 <?php
-$album = $_GET['album'] ?? '';
-$folder = __DIR__ . "/assets/img/" . $album;
+// $album = $_GET['album'] ?? '';
+// $folder = __DIR__ . "/assets/img/" . $album;
 
-$files = [];
-if (is_dir($folder)) {
-    foreach (scandir($folder) as $file) {
-        if (
-            preg_match('/\.(jpg|jpeg|png|gif)$/i', $file) && 
-            strtolower($file) !== 'cover.png' // exclude cover.png
-        ) {
-            $files[] = $file;
-        }
-    }
-}
+// $files = [];
+// if (is_dir($folder)) {
+//     foreach (scandir($folder) as $file) {
+//         if (
+//             preg_match('/\.(jpg|jpeg|png|gif)$/i', $file) && 
+//             strtolower($file) !== 'cover.png' // exclude cover.png
+//         ) {
+//             $files[] = $file;
+//         }
+//     }
+// }
 
-header('Content-Type: application/json');
-echo json_encode($files);
-exit;
+// header('Content-Type: application/json');
+// echo json_encode($files);
+// exit;
 ?>
 
