137ddbe065d8b4fe6407a02288909ac14df2df2f
Nurul Izzah <mephistomum@gmail.com>
2025-10-11 08:38:44 +0800

    update image

--- album.js
+++ album.js
@@ -35,8 +35,9 @@ function renderAlbums(filterArtist = "All") {
     let albumArtist = "";
     if (["Radiant Brilliance","Goodcat Code","Valleydream Bloom","Passionate Appraisal","Innocent Birdcage","Alternative Darkborne","Abyssal Mark","Abyssal Blossom","Long-Awaited Reverly","Absurdity Unfolds","Midnight Stealth", "Magnum Opus", 
       "Night of Secrecy", "Where Heart Lives", "Lovespeed Ride"].includes(albumName)) albumArtist = "Sylus";
-    else if (["Immediate Disorder","Runaway Waves","Diviner's Stillness","Diviner's Hymn" ].includes(albumName)) albumArtist = "Zayne";
+    else if (["Absolute Zeal","Immediate Disorder","Runaway Waves","Diviner's Stillness","Diviner's Hymn" ].includes(albumName)) albumArtist = "Zayne";
     else if (["Mistsea Lament","Intertidal Zone" ].includes(albumName)) albumArtist = "Rafayel";
+    else if (["Between Stars", "Nightvow Requiem", "Nightvow Mooncurse","Misty Silhouette" ].includes(albumName)) albumArtist = "Xavier";
     else if (["No-Return Night" ].includes(albumName)) albumArtist = "Caleb";
     else albumArtist = "Other";
 
